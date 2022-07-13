744f7cfd1e985263e2a9addfdcae84251b3b8faa
DannyDiep963 <danny.963.dk@gmail.com>
2022-07-13 08:15:50 -0700

    Create nopass.sh

---
+++ nopass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo ${REQUIRED_PKG[@]}
+if [ "" = grep '$USER ALL=(ALL) NOPASSWD:ALL' ]; then
+  echo "Permissionless sudo is unabled. Activating mode..."
+  sudo bash -c 'echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoer.d/99_sudo_include_file'
+else
+  echo "Permissionless sudo is enabled"
+fi
